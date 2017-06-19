c74400eda7050835f25a09e409c869c682d2beb8
RashiSah <rashi.747@gmail.com>
2017-06-19 16:38:37 +0530

    config files

---
+++ config.py
@@ -0,0 +1,6 @@
+
+ACCESS_TOKEN = 'HvM2t16WJxfEjc-CrBIBIU6SjNE'
+CLIENT_ID = 'ucu7r5LAttct3Q'
+CLIENT_SECRET  = 'xyY0jj4rwnKpXABgJyYpjbLj4d8'
+AUTH_URL = 'https://www.reddit.com/api/v1/authorize?scope=identity&duration=permanent&redirect_uri=http%3A%2F%2Flocalhost%3A8080&client_id=SI8pN3DSbt0zor&state=...&response_type=code'
+
---
+++ config.sample.py
@@ -0,0 +1,6 @@
+
+ACCESS_TOKEN = 'qwer'
+CLIENT_ID = 'jdtjg'
+CLIENT_SECRET  = 'teeshyr'
+AUTH_URL = 'http://qqq'
+
