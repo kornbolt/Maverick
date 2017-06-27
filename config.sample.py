24b571be0021941da13a9a7de9a697fb8b3d6cab
Rashi Sah <rashi.747@gmail.com>
2017-06-27 18:35:44 +0530

    Update config.sample.py

--- config.sample.py
+++ config.sample.py
@@ -1,6 +1,11 @@
-
 ACCESS_TOKEN = 'qwer'
 CLIENT_ID = 'jdtjg'
 CLIENT_SECRET  = 'teeshyr'
 AUTH_URL = 'http://qqq'
+Username = 'user'
+password = 'pass'
+
+'''You need to configure the 'praw.ini' file which is located in /home/user/.local/lib/python3.5/site-packages/praw/praw.ini ,
+(according to the OS directories)
+At the end of the file, append all the configurations you have got above.'''
 
