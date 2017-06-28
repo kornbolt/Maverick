2b229a096b431836918b1894ee0f4eebc9a77b8d
Rashi Sah <rashi.747@gmail.com>
2017-06-28 21:45:13 +0530

    Added filename

--- config.sample.py
+++ config.sample.py
@@ -4,6 +4,7 @@ CLIENT_SECRET  = 'teeshyr'
 AUTH_URL = 'http://qqq'
 Username = 'user'
 password = 'pass'
+FILENAME = "filename.txt"
 
 '''You need to configure the 'praw.ini' file which is located in /home/user/.local/lib/python3.5/site-packages/praw/praw.ini ,
 (according to the OS directories)
